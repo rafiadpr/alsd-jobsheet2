9f0fb1552d7331d90d5296c0acdf626b1ab84b2e
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-18 14:34:43 +0700

    change on main dosen23

--- DosenMain23.java
+++ DosenMain23.java
@@ -3,8 +3,8 @@ public class DosenMain23 {
 
     public static void main(String[] args) {
         Dosen23 dosen1 = new Dosen23();
-        dosen1.idDosen = "D001";
-        dosen1.nama = "Budi";
+        dosen1.idDosen = "01";
+        dosen1.nama = "Dosen 1";
         dosen1.statusAktif = true;
         dosen1.tahunBergabung = 2010;
         dosen1.bidangKeahlian = "Pemrograman";
@@ -15,11 +15,11 @@ public class DosenMain23 {
         dosen1.ubahKeahlian("Struktur Data");
         dosen1.tampilkanInformasi();
 
-        Dosen23 dosen2 = new Dosen23("D002", "Ani", true, 2015, "Pemrograman");
+        Dosen23 dosen2 = new Dosen23("02", "Dosen 2", true, 2015, "Pemrograman");
         dosen2.ubahKeahlian("Algoritma");
         dosen2.tampilkanInformasi();
 
-        Dosen23 dosen3 = new Dosen23("D003", "Cici", false, 2018, "Basis Data");
+        Dosen23 dosen3 = new Dosen23("03", "Dosen 3", false, 2018, "Basis Data");
         dosen3.hitungMasaKerja(2021);
         dosen3.tampilkanInformasi();
     }
